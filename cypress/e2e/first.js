f50090b51d31e7fd699c40c74bdd19eaa27f7a83
Valerii <mandryka.valerii@gmail.com>
2022-08-17 18:02:59 +0300

    modified:   cypress/e2e/first.js

--- cypress/e2e/first.js
+++ cypress/e2e/first.js
@@ -5,5 +5,5 @@ Given('User opens main page', () => {
     cy.visit('https://www.google.com/');
 })
 Given('Type "Google"', () => {
-    cy.xpath('[type="text"]').type('Google');
+    cy.get('[type="text"]').type('Google');
 })
